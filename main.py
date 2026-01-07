8562a513a3f2a1854a7d9016bd4f16531c2410d0
ChaseChen <228460677+ChaseanChen@users.noreply.github.com>
2026-01-07 21:40:07 +0800

    implement the reload command logic

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ plugin_context = {
     "data": []}
     # 允许插件继续补充相关数据
 loaded_plugins = {}
+loaded_modules = {}
 
 def load_func(filename):
     """
@@ -31,8 +32,11 @@ def load_confirmed_func(filename, name, path):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        run_func(mod, filename)
-        print(f"[+] {filename} load successed")
+        
+        loaded_modules[name] = mod
+        # run_func(mod, filename)
+        # print(f"[+] {filename} load successed")
+
         if hasattr(mod, "Plugin"):
             plugin_instance = mod.Plugin(plugin_context)
             loaded_plugins[name] = plugin_instance
@@ -100,7 +104,49 @@ if __name__ == "__main__":
                     print(f"[!] 找不到插件： {name}")
             except IndexError:
                 print("[!] 语法错误")
-        
+                
+        elif cmd.startswith("reload "):
+            try:
+                name = cmd.split(" ", 1)[1].strip()
+                
+                # 1. 检查是否加载过
+                if name not in loaded_modules:
+                    print(f"[!] 模块 {name} 未加载")
+                    print("请使用load命令")
+                    continue
+                
+                print(f"[*] 正在热重载： {name} ...")
+                
+                # 2. 停止就的插件实例
+                # 如果不停止，就的线程可能还在进行，变成僵尸
+                if name in loaded_plugins:
+                    try:
+                        old_plugin = loaded_plugins[name]
+                        old_plugin.stop()
+                        del loaded_plugins[name] # 从内存中移除旧的实例
+                    except Exception as e:
+                        print(f"[!] 停止旧实例失败者: {e}")
+                
+                # 3. 强制重载模块对象
+                # 这会重新读取 .py 文件，更新内存中的类定义
+                mod = loaded_modules[name]
+                new_mod = importlib.reload(mod)
+                loaded_modules[name] = new_mod
+                # 更新模块引用
+                
+                # 4. 重新实例化并启动
+                if hasattr(new_mod, "Plugin"):
+                    new_instance = new_mod.Plugin(plugin_context)
+                    loaded_plugins[name] = new_instance
+                    new_instance.start()
+                    print(f"[+] {name} 热重载完成")
+                    
+            except IndexError:
+                print("[!] 用法： reloaded <插件名>")
+            
+            except Exception as e:
+                print(f"[-] 重载失败： {e}")
+                traceback.print_exc()
                 
         elif cmd == "data":
             # 查看当前的共享上下文数据
