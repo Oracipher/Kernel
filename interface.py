ed3c133e5c341ad5022d860694f5899f3719f390
ChaseChen <228460677+ChaseanChen@users.noreply.github.com>
2026-01-22 18:35:02 +0800

    deleted some docs

--- interface.py
+++ interface.py
@@ -1,23 +1,16 @@
 # interface.py
-from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from api import PluginAPI
+from abc import ABC,abstractmethod
 
 class IPlugin(ABC):
-    """
-    插件接口基类
-    """
-    def __init__(self, api: 'PluginAPI') -> None:
-        self.api = api
-        
+    
     @abstractmethod
     def start(self) -> None:
-        """插件启动入口"""
+        """实现启动"""
         pass
     
     @abstractmethod
     def stop(self) -> None:
-        """插件停止清理"""
+        """用来停止"""
         pass
+    
